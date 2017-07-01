da08935f3cb831e9b25dd37f57d0ff3b4ddf355f
Edwin Chen <edwinchen85@gmail.com>
2017-07-01 22:01:04 +0800

    JSX comment

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
+    // Can use JS comment format here
     return (
       <form className="store-selector">
+        { /* JSX Comment */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store →</button>
