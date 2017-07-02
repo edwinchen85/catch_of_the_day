26c58cfc4802cc10a9c8c4aa8ec9df7b99739612
edwinchen85 <edwinchen85@gmail.com>
2017-07-02 22:08:40 +0800

    Add defaultValue props that get random fun names

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -8,7 +8,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
         { /* JSX Comment */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store →</button>
       </form>
     )
