70c81e099856aabfd2f86d04c000356261017596
edwinchen85 <edwinchen85@gmail.com>
2017-07-02 21:38:40 +0800

    Import { BrowserRouter, Match, Miss } from react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 
