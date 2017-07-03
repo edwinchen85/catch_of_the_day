ff21541ac3efa39bf7d67edd3cac31a46994c16a
edwinchen85 <edwinchen85@gmail.com>
2017-07-03 22:06:00 +0800

    Add the rest of JSX

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -6,6 +6,12 @@ class Fish extends React.Component {
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{details.price}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
       </li>
     )
   }
