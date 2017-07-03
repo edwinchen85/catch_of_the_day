f719eaeaf87f2b64ca276dbd5df49ed4aab8fc4b
Edwin Chen <edwinchen85@gmail.com>
2017-07-03 17:29:34 +0800

    Add fish object with input refs

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -4,6 +4,14 @@ class AddFishForm extends React.Component {
   createFish(event) {
     event.preventDefault();
     console.log('Gonna make some fish!');
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value,
+    };
+    console.log(fish);
   }
 
   render() {
