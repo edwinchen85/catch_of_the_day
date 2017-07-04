6998df8e482de850becc55b9e0ca5290e8b51a0f
Edwin Chen <edwinchen85@gmail.com>
2017-07-04 10:32:30 +0800

    Use Rebase to allow our web app to connect to Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCSiS3eC5aHfSzrKG097VuK551ShKOW1gQ",
+  authDomain: "catch-of-the-day-edwin-chen.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-edwin-chen.firebaseio.com"
+});
+
+export default base;
