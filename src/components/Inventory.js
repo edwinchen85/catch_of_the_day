1885e29f1ae1b936c97f2fb2c8d8493aa87efa4c
Edwin Chen <edwinchen85@gmail.com>
2017-07-04 18:43:16 +0800

    Add onChange listener to each input that will trigger handleChange() method

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -11,14 +11,14 @@ class Inventory extends React.Component {
     const fish = this.props.fishes[key];
     return (
       <div className="fish-edit" key={key}>
-        <input type="text" name="name" value={fish.name} placeholder="Fish Name" />
-        <input type="text" name="price" value={fish.price} placeholder="Fish Price" />
-        <select type="text" name="status" value={fish.status} placeholder="Fish Status">
+        <input type="text" name="name" value={fish.name} placeholder="Fish Name" onChange={e => this.handleChange(e, key)} />
+        <input type="text" name="price" value={fish.price} placeholder="Fish Price" onChange={e => this.handleChange(e, key)} />
+        <select type="text" name="status" value={fish.status} placeholder="Fish Status" onChange={e => this.handleChange(e, key)}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc"></textarea>
-        <input type="text" name="image" value={fish.image} placeholder="Fish Image" />
+        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc" onChange={e => this.handleChange(e, key)}></textarea>
+        <input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={e => this.handleChange(e, key)} />
       </div>
     )
   }
